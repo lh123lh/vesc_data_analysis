59e7ded3365435943ce16ceefa7becde9fa6d2e6
lh123lh <184591403@qq.com>
2024-12-18 15:26:17 +0800

    添加action 自动打包功能

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,12 +2,13 @@
 extern crate slcan;
 
 mod cmds;
-mod process_csv;
 mod motor;
+mod process_csv;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
